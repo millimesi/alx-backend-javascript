a82a14b7d2a0e33710e5da1f65a88d3523035680
millimesi <millionmesi1@gmail.com>
2024-06-07 18:49:31 +0300

    Task 2 modified with the help of Magret

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,8 @@
-export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-    return { status: 200, body: 'success' };
-  })
+export default function getFullResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
     .catch(() => new Error());
 }
