b2d405d5a3798271879acb6f0673e1abba4628d3
millimesi <millionmesi1@gmail.com>
2024-06-11 10:16:41 +0300

    Task 4 lint error and a bug fixed

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,39 +1,44 @@
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
 export default class Pricing {
-    constructor(amount, currency) {
-        if (typeof(amount) !== 'number') {
-            throw new Error('Amount must be a Number');
-        }
-        if (!(currency instanceof Currency)) {
-        throw new Error('currency must be an instance of  Currency');
-        }
-        this._amount = amount;
-        this._currency = currency;
+  constructor(amount, currency) {
+    if (typeof (amount) !== 'number') {
+      throw new Error('Amount must be a number');
     }
-    get amount() {
-        return this._amount;
-    }  
-    set amount(amount) {
-        if (typeof(amount) !== 'number') {
-            throw new Error('Amount must be a Number');
-        }
-        this._amount = amount;
+    if (!(currency instanceof Currency)) {
+      throw new Error('Currency must be an instance of  Currency');
     }
-    get currency() {
-        if (!(currency instanceof Currency)) {
-            throw new Error('currency must be an instance of  Currency');
-        }
-        return this._currency;
-    }
-    set currency(currency) {
-        this._currency = currency;
-    }
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-    displayFullPrice() {
-        return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof (amount) !== 'number') {
+      throw new Error('Amount must be a number');
     }
-    static convertPrice(amount, conversionRate) {
-        return amount * conversionRate;
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new Error('Currency must be an instance of  Currency');
     }
-} 
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
