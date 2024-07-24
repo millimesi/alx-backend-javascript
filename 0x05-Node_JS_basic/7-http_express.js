b6af4f822a7ccb2a2c058286e1a725e49c9266d6
millimesi <millionmesi1@gmail.com>
2024-07-24 18:46:20 +0300

    Task 6 and 7 is added

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+      if (lines.length <= 1) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      lines.shift();
+
+      const studentsByField = {};
+
+      for (const line of lines) {
+        const parts = line.split(',');
+
+        if (parts.length === 4) {
+          const firstname = parts[0];
+          const field = parts[3];
+
+          if (!studentsByField[field]) {
+            studentsByField[field] = [];
+          }
+
+          studentsByField[field].push(firstname);
+        }
+      }
+
+      const fieldValues = Object.values(studentsByField);
+      const totalStuden = fieldValues.reduce((acc, fieldStudents) => acc + fieldStudents.length, 0);
+
+      let result = `Number of students: ${totalStuden}\n`;
+      for (const [field, students] of Object.entries(studentsByField)) {
+        result += `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}\n`;
+      }
+
+      resolve(result.trim());
+    });
+  });
+}
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  try {
+    const studentInfo = await countStudents(process.argv[2]);
+
+    res.send(`This is the list of our students\n${studentInfo}`);
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
+app.listen(1245, () => {
+  console.log('server is running on port 1245');
+});
+
+module.exports = app;
