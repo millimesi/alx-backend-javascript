012d0d98d832490b8de38731625a8b947c891197
millimesi <millionmesi1@gmail.com>
2024-07-25 12:35:25 +0300

    Task 1 is added

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,20 @@
+function calculateNumber(type, a, b) {
+    const roundA = Math.round(a);
+    const roundB = Math.round(b);
+
+    if (type === 'SUM') {
+        return (roundA + roundB );
+    } else if (type === 'SUBTRACT') {
+        return (roundB - roundA);
+    } else if (type === 'DIVIDE') {
+        if (roundB === 0) {
+            return 'Error'
+        } else {
+            return (roundA / roundB)
+        }
+    } else {
+        return 'Type should be SUM, SUBSTRACT or DIVIDE'
+    }
+}
+
+module.exports = calculateNumber;
