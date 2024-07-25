f7fb36f2e79c6a09e5f4bfaf530f2fe394f2d20d
millimesi <millionmesi1@gmail.com>
2024-07-25 10:52:11 +0300

    Task zero is added

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+    it('Test adding whole numbers', () => {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+    });
+
+    it('Test adding whole number and above 0.5 float', ()=> {
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+
+    it('Test adding under 0.5 float and above 0.5 float', () => {
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+
+    it('Test adding number above 0.5 foat', () => {
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
+})
